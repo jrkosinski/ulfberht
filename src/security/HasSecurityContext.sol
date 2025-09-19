bc66e737e905f266c496308eb8e919d35090e1fd
jrkosinski <john.kosinski@gmail.com>
2025-09-19 10:25:48 +0700

    reorganized contracts

---
+++ src/security/HasSecurityContext.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "./interfaces/ISecurityContext.sol";
+import "./SecurityRoles.sol";
+
+/**
+ * @title HasSecurityContext
+ * 
+ * @dev 
+ */
+abstract contract HasSecurityContext {
+    ISecurityContext public securityContext;
+
+    bool private initialized = false;
+
+    error UnauthorizedAccess(bytes32 roleId, address addr);
+    error ZeroAddressArgument();
+
+    event SecurityContextSet(address indexed caller, address indexed securityContext);
+
+    modifier onlyRole(bytes32 role) {
+        if (!securityContext.hasRole(role, msg.sender)) {
+            revert UnauthorizedAccess(role, msg.sender);
+        }
+        _;
+    }
+
+    function getSecurityContext() external virtual view returns (ISecurityContext) {
+        return securityContext;
+    }
+
+    function setSecurityContext(ISecurityContext _securityContext) external virtual onlyRole(SecurityRoles.ADMIN_ROLE) {
+        _setSecurityContext(_securityContext);
+    }
+
+    function _setSecurityContext(ISecurityContext _securityContext) internal {
+        if (address(_securityContext) == address(0)) revert ZeroAddressArgument();
+
+        if (!initialized) {
+            initialized = true;
+        } else {
+            if (!_securityContext.hasRole(SecurityRoles.ADMIN_ROLE, msg.sender)) {
+                revert("Caller is not admin");
+            }
+        }
+
+        if (securityContext != _securityContext) {
+            securityContext = _securityContext;
+            emit SecurityContextSet(msg.sender, address(_securityContext));
+        }
+    }
+}
---
+++ src/utility/SystemSettings.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "./HasSecurityContext.sol";
+import "./interfaces/ISystemSettings.sol";
+import "./SecurityRoles.sol";
+
+/**
+ * @title SystemSettings
+ *
+ * Holds global settings, to be set only by privileged parties, for all escrow contracts to read.
+ */
+contract SystemSettings is HasSecurityContext, ISystemSettings {
+    address private _vaultAddress;
+    uint256 private _feeBps;
+
+    // EVENTS
+    event VaultAddressChanged(address newAddress, address changedBy);
+    event FeeBpsChanged(uint256 newValue, address changedBy);
+
+    /**
+     * Address of the vault to which fees are paid.
+     */
+    function vaultAddress() external view returns (address) {
+        return _vaultAddress;
+    }
+
+    /**
+     * Amount in basis points, indicating the portion of payments to be separated and paid to the vault as fees.
+     */
+    function feeBps() external view returns (uint256) {
+        return _feeBps;
+    }
+
+    /**
+     * Constructor.
+     *
+     * Emits:
+     * - {HasSecurityContext-SecurityContextSet}
+     *
+     * Reverts:
+     * - {ZeroAddressArgument} if the securityContext address is 0x0.
+     * - 'InvalidVaultAddress' if the given vault address is zero.
+     *
+     * @param securityContext Contract which will define & manage secure access for this contract.
+     * @param vaultAddress_ Recipient of the extracted fees.
+     * @param feeBps_ Amount of fees charged in basis points.
+     */
+    constructor(ISecurityContext securityContext, address vaultAddress_, uint256 feeBps_) {
+        _setSecurityContext(securityContext);
+        if (vaultAddress_ == address(0)) {
+            revert("InvalidVaultAddress");
+        }
+        _vaultAddress = vaultAddress_;
+        _feeBps = feeBps_;
+    }
+
+    /**
+     * Sets the address to which fees are sent.
+     *
+     * Emits:
+     * - {VaultAddressChanged}
+     *
+     * Reverts:
+     * - 'UnauthorizedAccess' if caller is not authorized as SYSTEM_ROLE.
+     * - 'InvalidValue' if the given address is invalid (zero address).
+     *
+     * @param vaultAddress_ The new address.
+     */
+    function setVaultAddress(address vaultAddress_) public onlyRole(SecurityRoles.SYSTEM_ROLE) {
+        if (_vaultAddress != vaultAddress_) {
+            if (vaultAddress_ == address(0)) {
+                revert("InvalidValue");
+            }
+            _vaultAddress = vaultAddress_;
+            emit VaultAddressChanged(_vaultAddress, msg.sender);
+        }
+    }
+
+    /**
+     * Sets the fee basis points (feeBps).
+     *
+     * Emits:
+     * - {FeeBpsChanged}
+     *
+     * Reverts:
+     * - 'UnauthorizedAccess' if caller is not authorized as SYSTEM_ROLE.
+     *
+     * @param feeBps_ The new value for fee in basis points.
+     */
+    function setFeeBps(uint256 feeBps_) public onlyRole(SecurityRoles.SYSTEM_ROLE) {
+        if (_feeBps != feeBps_) {
+            _feeBps = feeBps_;
+            emit FeeBpsChanged(_feeBps, msg.sender);
+        }
+    }
+}
