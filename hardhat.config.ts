76054359f82d4994bf50ade2df75b3234ced0917
jrkosinski <john.kosinski@gmail.com>
2025-09-21 14:23:29 +0700

    fixed fallback private keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import '@nomicfoundation/hardhat-toolbox';
 
 require('dotenv').config();
 
+const FALLBACK_PRIVATE_KEY =
+    'c0ffee254729296a45a3885630617005af6f3a571fe75275a9AC7E10F9d54979';
+
 const config: HardhatUserConfig = {
     solidity: {
         version: '0.8.28',
@@ -24,57 +27,48 @@ const config: HardhatUserConfig = {
             // You can configure different networks here if needed
         },
         sepolia: {
-            accounts: [
-                process.env.SEPOLIA_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
-            ],
+            accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY],
             chainId: 11155111,
             url: `https://sepolia.infura.io/v3/${process.env.INFURA_ID}`,
         },
         optimism: {
             accounts: [
-                process.env.OPTIMISM_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
+                process.env.OPTIMISM_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY,
             ],
             chainId: 10,
             url: `https://mainnet.optimism.io`,
         },
         ethereum: {
             accounts: [
-                process.env.OPTIMISM_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
+                process.env.OPTIMISM_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY,
             ],
             chainId: 1,
             url: `https://ethereum-rpc.publicnode.com`,
         },
         arbitrum: {
             accounts: [
-                process.env.OPTIMISM_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
+                process.env.OPTIMISM_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY,
             ],
             chainId: 42161,
             url: `https://arb1.arbitrum.io/rpc`,
         },
         base: {
             accounts: [
-                process.env.OPTIMISM_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
+                process.env.OPTIMISM_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY,
             ],
             chainId: 8453,
             url: `https://mainnet.base.org`,
         },
         polygon: {
             accounts: [
-                process.env.OPTIMISM_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
+                process.env.OPTIMISM_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY,
             ],
             chainId: 137,
             url: `https://polygon.api.onfinality.io/public`,
         },
         amoy: {
             accounts: [
-                process.env.OPTIMISM_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
+                process.env.OPTIMISM_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY,
             ],
             chainId: 80002,
             url: `https://rpc-amoy.polygon.technology`,
@@ -84,16 +78,10 @@ const config: HardhatUserConfig = {
             chainId: 11155420,
             gasPrice: 8000000000,
             gasMultiplier: 2,
-            accounts: [
-                process.env.SEPOLIA_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
-            ],
+            accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY],
         },
         optimism_sepolia: {
-            accounts: [
-                process.env.SEPOLIA_PRIVATE_KEY ??
-                    'c0ffee254729296a45a3885639AC7E10F9d54979',
-            ],
+            accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? FALLBACK_PRIVATE_KEY],
             chainId: 11155420,
             url: `https://sepolia.optimism.io`,
         },
